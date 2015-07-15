aa7fb71aae7067ee47ee275228d3ed6c7af880b8
gdonovan <gdonovan@optimalblue.com>
2015-07-15 15:35:47 -0500

    app.js

--- app.js
+++ app.js
@@ -1,3 +1,52 @@
 /**
  * Created by gdonovan on 7/15/2015.
  */
+// create the module and name it scotchApp
+var scotchApp = angular.module('scotchApp', ['ngRoute']);
+
+// configure our routes
+scotchApp.config(function($routeProvider) {
+    $routeProvider
+
+        // route for the home page
+        .when('/', {
+            templateUrl : 'views/home.html',
+            controller  : 'mainController'
+        })
+
+        // route for the about page
+        .when('/about', {
+            templateUrl : 'views/about.html',
+            controller  : 'aboutController'
+        })
+
+        // route for the contact page
+        .when('/contact', {
+            templateUrl : 'views/contact.html',
+            controller  : 'contactController'
+        })
+
+        .when('/quickSearch', {
+            templateUrl : 'views/quickSearch.html',
+            controller  : 'quickSearchController'
+        });
+});
+
+// create the controller and inject Angular's $scope
+scotchApp.controller('mainController', function($scope) {
+    // create a message to display in our view
+    $scope.message = 'Everyone come and see how good I look!';
+});
+
+scotchApp.controller('aboutController', function($scope) {
+    $scope.message = 'Look! I am an about page.';
+});
+
+scotchApp.controller('contactController', function($scope) {
+    $scope.message = 'Contact us! JK. This is just a demo.';
+});
+
+scotchApp.controller('quickSearchController', function($scope) {
+    // create a message to display in our view
+    $scope.message = 'Quick Search!';
+});
